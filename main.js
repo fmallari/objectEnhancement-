ec27a4bc14d5961eb14192691e6e7427d43178b2
fmallari <fc.mallari@gmail.com>
2023-01-27 23:04:46 -0800

    practice template

---
+++ main.js
@@ -0,0 +1,45 @@
+//Same keys and values
+
+function createInstructor(firstName, lastName){
+    return {
+      firstName: firstName,
+      lastName: lastName
+    }
+  }
+
+  //Same keys and values ES2015
+
+  function createInstructor(firstName, lastName) {
+    return {
+        firstName,
+        lastName
+    }
+  }
+
+  //Computed Property Names 
+
+  var favoriteNumber = 42;
+
+var instructor = {
+  firstName: "Colt"
+}
+
+instructor[favoriteNumber] = "That is my favorite!"
+
+//Computed Property Names ES2015
+
+//Object Methods 
+
+var instructor = {
+    firstName: "Colt",
+    sayHi: function(){
+      return "Hi!";
+    },
+    sayBye: function(){
+      return this.firstName + " says bye!";
+    }
+  }
+
+  //Object Methods ES2015
+
+  //createAnimal function 
